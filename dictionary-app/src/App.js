579eb1998a24f43cef705ef06de1c7a985dd7365
aliciaelena <alicia.nagele@gmx.at>
2022-04-10 18:06:19 +0200

    Initial commit

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type="button" class="btn btn-warning">
+          Hello
+        </button>
+      </header>
+    </div>
+  );
+}
