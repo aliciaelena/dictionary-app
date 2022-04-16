b25d38a54ec3177cd27f63c4b7aa416506b0de41
aliciaelena <alicia.nagele@gmx.at>
2022-04-16 13:10:31 +0200

    Integrated results from the API (word meaning & definitions)

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResult(response.data[0]);
   }
 
   function search(event) {
@@ -24,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleInput} />
       </form>
+      <Results results={result} />
     </div>
   );
 }
