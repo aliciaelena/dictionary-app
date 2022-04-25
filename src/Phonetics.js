9bc34ce832830572d310c154e1a4f05596b4991b
aliciaelena <alicia.nagele@gmx.at>
2022-04-25 22:28:25 +0200

    Added AudioPlayer for Phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Phonetics.css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      {props.phonetics.text}
-    </div>
-  );
+  if (props.phonetics && props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        <div className="phonetics">{props.phonetics.text}</div>
+        <AudioPlayer
+          style={{
+            width: "300px",
+            margin: "0px 0px 10px",
+          }}
+          customAdditionalControls={[]}
+          showJumpControls={false}
+          customVolumeControls={[]}
+          src={props.phonetics.audio}
+        />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
